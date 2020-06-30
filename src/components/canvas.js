93f8f9f8b9bfee949551574a28765df9851b39b7
humzaahmad906 <humzaahmad9066@gmail.com>
2020-06-30 02:16:23 -0700

    basic functionality of canvas fabric added

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -2,14 +2,22 @@ import React, {Component} from 'react'
 import {fabric} from 'react-fabricjs'
 class DrawingCanvas extends Component{
     constructor(){
-        this.refs = React.createRef()
+        
+
+    }
+    componentDidMount(){
+        this.canvas = new fabric.Canvas('c')
+        this.canvas.setWidth = 500
+        this.canvas.setHeight = 500
+        this.canvas.setState({
+            "backgroundcolor": "#000"
+        })
+        this.canvas.render()
     }
     render(){
         return (
             <div>
-                <canvas id = 'c'
-                ref = {this.refs}
-                >
+                <canvas id = 'c'>
 
                 </canvas>
             </div>
